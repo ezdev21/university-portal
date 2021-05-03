12c4d27bd28015d9ddf84838a55960fedfaedccb
gech11 <getachewfikadu77@gmail.com>
2021-05-03 10:53:39 +0300

    initial commit

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,10 +5,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Student Login') }}</div>
+                <div class="card-header">Admin</div>
 
                 <div class="card-body">
-                    <form method="POST" action="{{ route('student.login') }}">
+                    <form method="POST" action="{{ route('admin.login') }}">
                         @csrf
 
                         <div class="form-group row">
