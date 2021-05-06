b6adc7ea34a740a1a5cfd7f21fe11e3c37f78ee9
gech11 <getachewfikadu77@gmail.com>
2021-05-06 10:54:38 +0300

    initial commit

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Authenticatable as AuthenticableTrait;
 
-class Admin extends Model
+class Admin extends Model implements Authenticatable
 {
-    use HasFactory;
+    use HasFactory,AuthenticableTrait;
+    protected $guard='teacher';
+    protected $fillable=['name','email','password'];
+
 }
