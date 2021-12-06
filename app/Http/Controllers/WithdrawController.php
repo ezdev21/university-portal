447a65fa3e48d430016ed9e74024e2ae2c72b6ac
gech11 <getachewfikadu77@gmail.com>
2021-12-07 00:02:30 +0300

    initial commit

---
+++ app/Http/Controllers/WithdrawController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Withdraw;
+use App\Http\Requests\StoreWithdrawRequest;
+use App\Http\Requests\UpdateWithdrawRequest;
+
+class WithdrawController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreWithdrawRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreWithdrawRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Withdraw  $withdraw
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Withdraw $withdraw)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Withdraw  $withdraw
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Withdraw $withdraw)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateWithdrawRequest  $request
+     * @param  \App\Models\Withdraw  $withdraw
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateWithdrawRequest $request, Withdraw $withdraw)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Withdraw  $withdraw
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Withdraw $withdraw)
+    {
+        //
+    }
+}
