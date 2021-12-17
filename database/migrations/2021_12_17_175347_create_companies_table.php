ef2443647de51f0fc931a0e5ee8ecc9de58a5b79
gech11 <getachewfikadu77@gmail.com>
2021-12-17 20:59:24 +0300

    internship companies added

--- database/migrations/2021_12_17_175347_create_companies_table.php
+++ database/migrations/2021_12_17_175347_create_companies_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInternshipsTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateInternshipsTable extends Migration
      */
     public function up()
     {
-        Schema::create('internships', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('student_id');
-            $table->foreignId('company_id');
-            $table->longText('report');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateInternshipsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('internships');
+        Schema::dropIfExists('companies');
     }
 }
