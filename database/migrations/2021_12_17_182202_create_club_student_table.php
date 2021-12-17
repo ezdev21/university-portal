3d82e6b6c84de0b047260386cc801c5524852aab
gech11 <getachewfikadu77@gmail.com>
2021-12-17 21:23:30 +0300

    student and club polymorphic relationship created

---
+++ database/migrations/2021_12_17_182202_create_club_student_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClubStudentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('club_student', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('club_id');
+            $table->foreignId('student_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('club_student');
+    }
+}
