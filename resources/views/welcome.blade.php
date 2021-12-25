7fe6478f04043c5e302bfcb9c1a8b9a0e40c1d77
gech11 <getachewfikadu77@gmail.com>
2021-12-25 09:00:22 -0500

    layout changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,10 +21,86 @@
         </style>
     </head>
     <body>
-      <div class="bg-blue-400">
-       <a class="text-white no-underline text-xl" href="{{route('student.login')}}">student</a>
-       <a class="text-white no-underline text-xl" href="{{route('teacher.login')}}">teacher</a>
-       <a class="text-white no-underline text-xl" href="{{route('admin.login')}}">admin</a>   
-      </div>  
+      <div class="">
+       <div>
+           navigation
+       </div>
+       <div class="flex">
+         <div class="w-1/2">
+
+         </div>
+         <div class="w-1/2">
+          <div class="bg-social-media w-screen h-screen bg-repeat flex justify-center items-center">
+           <main class="sm:container sm:mx-auto sm:max-w-lg sm:mt-10">
+            <div class="flex">
+             <div class="w-full">
+              <section class="flex flex-col break-words bg-white sm:border-1 sm:rounded-md sm:shadow-sm sm:shadow-lg">
+                <header class="font-semibold bg-gray-100 text-gray-700 py-3 text-center sm:py-3 sm:rounded-t-md text-2xl text-first">
+                    Login to out liberary
+                </header>
+                <form class="w-full px-6 space-y-6 sm:px-10 sm:space-y-8" method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="flex flex-wrap">
+                        <label for="email" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                            {{ __('E-Mail Address') }}:
+                        </label>
+                        <input id="email" type="email"
+                            class="form-input w-full @error('email') border-red-500 @enderror text-xl text-first my-2 w-full text-xl p-2 rounded-xl border-2 border-gray-300" name="email"
+                            value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
+                    <div class="flex flex-wrap">
+                        <label for="password" class="block text-gray-700 text-sm font-bold mb-2 sm:mb-4">
+                            {{ __('Password') }}:
+                        </label>
+                        <input id="password" type="password"
+                            class="form-input w-full @error('password') border-red-500 @enderror text-xl text-first my-2 w-full text-xl p-2 rounded-xl border-2 border-gray-300" name="password"
+                            required>
+                        @error('password')
+                        <p class="text-red-500 text-xs italic mt-4">
+                            {{ $message }}
+                        </p>
+                        @enderror
+                    </div>
+                    <div class="flex items-center">
+                        <label class="inline-flex items-center text-sm text-gray-700 text-xl" for="remember">
+                            <input type="checkbox" name="remember" id="remember" class="form-checkbox"
+                                {{ old('remember') ? 'checked' : '' }}>
+                            <span class="ml-2">{{ __('Remember Me') }}</span>
+                        </label>
+                        @if (Route::has('password.request'))
+                        <a class="text-sm text-first hover:text-blue-700 whitespace-no-wrap no-underline hover:underline ml-auto text-xl"
+                            href="{{ route('password.request') }}">
+                            {{ __('Forgot Your Password?') }}
+                        </a>
+                        @endif
+                    </div>
+                    <div class="flex flex-wrap">
+                        <button type="submit"
+                        class="w-full select-none font-bold whitespace-no-wrap p-3 rounded-lg text-base leading-normal no-underline text-gray-100 bg-first hover:bg-blue-700 sm:py-4">
+                            Login
+                        </button>
+                        @if (Route::has('register'))
+                        <p class="w-full text-center text-gray-700 my-6 sm:my-8 text-xl">
+                            {{ __("Don't have an account?") }}
+                            <a class="text-first hover:text-blue-700 no-underline hover:underline" href="{{ route('register') }}">
+                                {{ __('Register') }}
+                            </a>
+                        </p>
+                        @endif
+                    </div>
+                </form>
+              </section>
+            </div>
+           </div>
+          </main>
+         </div> 
+         </div>
+       </div>
+      </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,9 @@ use App\Http\Controllers\Admin\AdminPasswordRestController;
 use App\Http\Controllers\Admin\AdminForgotPasswordController;
 use App\Http\Controllers\PlacementController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
-    return view('home');
-})->middleware('auth');
+    return view('welcome');
+});
 
 Auth::routes();
 
