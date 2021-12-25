485b8645b82886eed9c1143e4cd997afffc363bd
gech11 <getachewfikadu77@gmail.com>
2021-12-25 07:28:49 -0500

    school scholarships added

---
+++ app/Http/Controllers/ScholarshipController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreScholarshipRequest;
+use App\Http\Requests\UpdateScholarshipRequest;
+use App\Models\Scholarship;
+
+class ScholarshipController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreScholarshipRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreScholarshipRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Scholarship  $scholarship
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Scholarship $scholarship)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Scholarship  $scholarship
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Scholarship $scholarship)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateScholarshipRequest  $request
+     * @param  \App\Models\Scholarship  $scholarship
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateScholarshipRequest $request, Scholarship $scholarship)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Scholarship  $scholarship
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Scholarship $scholarship)
+    {
+        //
+    }
+}
